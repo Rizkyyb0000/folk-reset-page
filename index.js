270747d72ac159297e73beee44dea22ee6804a09
Rizkyyb0000 <ryanda0000@gmail.com>
2025-04-23 21:55:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 const app = express();
 
-// Menyajikan folder public untuk file HTML, CSS, JS
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Jika ada route lain, tetap arahkan ke index.html
+
 app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Export untuk digunakan Vercel
+
 module.exports = app;
